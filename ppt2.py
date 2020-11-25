2f0f9a57227297ba6c41ad25dc657ae8268f158d
MuhammadBisma <bismaputra1986@gmail.com>
2020-11-25 06:46:24 -0800

    Update README.md

--- ppt2.py
+++ ppt2.py
@@ -1,122 +1,34 @@
-print("###############################################################")
-print("|                     Program Input Nilai                     |")
-print("###############################################################")
+nama = input("NAMA: ")
+nim = input("NIM: ")
+tugas = int(input("NILAI TUGAS: "))
+uts = int(input("NILAI UTS: "))
+uas = int(input("NILAI UAS: "))
 
-data = {}
+if (input("TAMBAHKAN DATA (Y/T)?") == ''):
+    ulang = False
 
-while True:
-    print("\n")
-    menu = input("(L) Lihat, (T) Tambah, (U) Ubah, (H) Hapus, (C) Cari, (K) Keluar: ")
-    print("\n")
+nilai = []
+ulang = True
 
-    # Keluar
-    if menu.lower() == 'k':
-        break
+while ulang:
+    nama = input("Masukkan Nama: ")
+    nim = input("Masukkan NIM: ")
+    tugas = int(input("Masukkan Nilai Tugas: "))
+    uts = int(input("Masukkan Nilai UTS: "))
+    uas = int(input("Masukkan Nilai UAS: "))
+    akhir = (tugas * 30/100) + (uts * 35/100) + (uas * 35/100)
 
-    # Lihat
-    elif menu.lower() == 'l':
-        print("Daftar Nilai:")
-        print("_________")
-        print("| No |      Nama      |    NIM    | Tugas |  UTS  |  UAS  | Akhir |")
-        print("===================================================================")
-        no = 1
-        for tabel in data.values():
-            print("| {0:2} | {1:14} | {2:9} | {3:5} | {4:5} | {5:5} | {6:5} |".format
-                  (no, tabel[0],
-                   tabel[1], tabel[2],
-                   tabel[3], tabel[4], tabel[5]))
-            no += 1
+    nilai.append([nama, nim, tugas, uts, uas, int(akhir)])
+    if (input("Tambah data (y/t)?") == 't'):
+        ulang = False
 
-    # Tambah
-    elif menu.lower() == 't':
-        print("Masukan data mahasiswa")
-        print("...")
-        nama = input("Masukan nama: ")
-        nim = input("Masukan NIM: ")
-        nilai_tugas = int(input("Masukan nilai tugas: "))
-        nilai_uts = int(input("Masukan nilai UTS: "))
-        nilai_uas = int(input("Masukan nilai UAS: "))
-        nilai_akhir = (nilai_tugas)*30/100 + (nilai_uts)*35/100 + (nilai_uas)*35/100
-        data[nama] = [nama, nim, nilai_tugas, nilai_uts, nilai_uas, nilai_akhir]
-        print('\nData berhasil di tambah!')
-        print("_________")
-        print("| No |      Nama      |    NIM    | Tugas |  UTS  |  UAS  | Akhir |")
-        print("===================================================================")
-        no = 1
-        for tabel in data.values():
-            print("| {0:2} | {1:14} | {2:9} | {3:5} | {4:5} | {5:5} | {6:5} |".format
-                  (no, tabel[0],
-                   tabel[1], tabel[2],
-                   tabel[3], tabel[4], tabel[5]))
-            no += 1
-
-    # Ubah
-    elif menu.lower() == 'u':
-        nama = input("Masukan nama untuk mengubah data: ")
-        if nama in data.keys():
-            print("Mau mengubah apa?")
-            sub_data = input("(Semua), (Nama), (NIM), "
-                         "(Tugas), (UTS), (UAS) : ")
-            if sub_data.lower() == "semua":
-                print("==========================")
-                print("Ubah data {}.".format(nama))
-                print("==========================")
-                data[nama][1] = input("Ubah NIM:")
-                data[nama][2] = int(input("Ubah Nilai Tugas: "))
-                data[nama][3] = int(input("Ubah Nilai UTS: "))
-                data[nama][4] = int(input("Ubah Nilai UAS: "))
-                data[nama][5] = data[nama][2] *30/100 + data[nama][3]*35/100 + data[nama][4] *35/100
-                print("\nBerhasil ubah data!")
-                print("_________")
-                print("| No |      Nama      |    NIM    | Tugas |  UTS  |  UAS  | Akhir |")
-                print("===================================================================")
-                no = 1
-                for tabel in data.values():
-                    print("| {0:2} | {1:14} | {2:9} | {3:5} | {4:5} | {5:5} | {6:5} |".format
-                        (no, tabel[0],
-                        tabel[1], tabel[2],
-                        tabel[3], tabel[4], tabel[5]))
-                    no += 1
-            elif sub_data.lower() == "nim":
-                data[nama][1] = input("NIM:")
-                print('Data berhasil di ubah!')
-            elif sub_data.lower() == "tugas":
-                data[nama][2] = int(input("Nilai Tugas: "))
-                print('Data berhasil di ubah!')
-            elif sub_data.lower() == "uts":
-                data[nama][3] = int(input("Nilai UTS: "))
-                print('Data berhasil di ubah!')
-            elif sub_data.lower() == "uas":
-                data[nama][4] = int(input("Nilai UAS: "))
-                print('Data berhasil di ubah!')
-            else:
-                print("menu tidak ditemukan.")
-
-        else:
-            print("'{}' tidak ditemukan.".format(nama))
-
-    # Cari
-    elif menu.lower() == 'c':
-        print("Mencari data: ")
-        print("=================================================")
-        nama = input("Masukan nama untuk mencari data: ")
-        if nama in data.keys():
-            print('\nResult')
-            print("Nama: {0}\nNIM : {1}\nNilai Tugas: {2}\nUTS: {3}\nUAS: {4}\nNilai akhir: {5}"
-                  .format(nama, data[nama][1],
-                                data[nama][2], data[nama][3],
-                                data[nama][4], data[nama][5]))
-        else:
-            print("'{}' tidak ditemukan.".format(nama))
-
-    # Hapus
-    elif menu.lower() == 'h':
-        nama = input("Masukan nama untuk menghapus sub_data : ")
-        if nama in data.keys():
-            del data[nama]
-            print("sub_data '{}' berhasil dihapus.".format(nama))
-        else:
-            print("'{}' tidak ditemukan.".format(nama))
-
-    else:
-        print("Upss ada yang salah, silahkan cek kembali.")
+print("\n                      Daftar Nilai Mahasiswa")
+print("==================================================================")
+print("|No. |     Nama     |    NIM    | Tugas |  UTS  |  UAS  |  Akhir |")
+print("==================================================================")
+i = 0
+for item in nilai:
+    i += 1
+    print("| {no:2d} | {nama:12s} | {nim:9s} | {tugas:5d} | {uts:5d} | {uas:5d} | {akhir:6.2f} |"
+          .format(no=i, nama=item[0], nim=item[1], tugas=item[2], uts=item[3], uas=item[4], akhir=item[5]))
+print("==================================================================")
